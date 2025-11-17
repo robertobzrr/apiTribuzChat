04fa2e29b579c6b54ece77618429af4f62e0788f
robertobzrr <robertobezerrafh@outlook.com>
2025-11-17 10:01:00 -0300

    usuario, grupo e tribo repositorys

---
+++ src/main/java/com/tribuzchat/api/repository/GrupoRepository.java
@@ -0,0 +1,9 @@
+package com.tribuzchat.api.repository;
+
+import com.tribuzchat.api.model.Grupo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GrupoRepository extends JpaRepository<Grupo, Long> {
+}
---
+++ src/main/java/com/tribuzchat/api/repository/UsuarioRepository.java
@@ -0,0 +1,9 @@
+package com.tribuzchat.api.repository;
+
+import com.tribuzchat.api.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+}
